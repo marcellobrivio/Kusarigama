8a9c180e82fa63aa6ecf7e56fed55f4a8137ec1f
marcellobrivio <marcello.brivio@gmail.com>
2015-04-29 16:52:21 +0200

    Kusarigama v2.1
    Improved markup for structured data. Better management of sticky posts
    in archive pages.

--- index.php
+++ index.php
@@ -6,19 +6,20 @@
 		
 							<article>
 								<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-									<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-									<p class="metadata">Posted on <?php the_time('F jS, Y'); ?> <?php if ( $user_ID ) : ?> &middot; <?php edit_post_link(); ?> <?php endif; ?>&middot; <?php comments_popup_link('No Comments', '1 Comment &raquo;', '% Comments &raquo;'); ?></p>
+									<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+									<p class="metadata"><span class="updated">Posted on <time pubdate datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time('F jS, Y'); ?></time></span> <?php if ( $user_ID ) : ?> &middot; <?php edit_post_link(); ?> <?php endif; ?>&middot; <?php comments_popup_link('No Comments', '1 Comment &raquo;', '% Comments &raquo;'); ?></p>
 									<div class="content">
-										<?php if (is_sticky()) : ?>
+										<?php if (is_sticky() && is_home()) : ?>
 											<?php the_excerpt(); ?>
 										<?php else : ?>
 											<?php the_content('[Read more...]'); ?>
 										<?php endif; ?>
 									</div>
-									<?php if (is_sticky()) : ?>
+									<?php if (is_sticky() && is_home()) : ?>
 										<p class="continue"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">Read the full story... &raquo;</a></p>
+										<p class="hidden author vcard"><a class="url fn" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></span></p>
 									<?php else : ?>
-										<p class="category">Author: <?php the_author_posts_link(); ?> &middot; Filed under: <?php the_category(', '); ?> <?php the_tags(' &middot; Tags: ', ', ', ''); ?></p>
+										<p class="category">Author: <span class="author vcard"><a class="url fn" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></span> &middot; Filed under: <?php the_category(', '); ?> <?php the_tags(' &middot; Tags: ', ', ', ''); ?></p>
 									<?php endif; ?>
 								</div>
 							</article>
--- single.php
+++ single.php
@@ -6,17 +6,18 @@
 		
 							<article>
 								<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-									<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-									<p class="metadata">Posted on <?php the_time('F jS, Y'); ?> <?php if ( $user_ID ) : ?> &middot; <?php edit_post_link(); ?> <?php endif; ?>&middot; <?php comments_popup_link('No Comments', '1 Comment &raquo;', '% Comments &raquo;'); ?></p>
+									<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+									<p class="metadata"><span class="updated">Posted on <time pubdate datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time('F jS, Y'); ?></time></span> <?php if ( $user_ID ) : ?> &middot; <?php edit_post_link(); ?> <?php endif; ?>&middot; <?php comments_popup_link('No Comments', '1 Comment &raquo;', '% Comments &raquo;'); ?></p>
 									<div class="content">
 										<?php the_content(); ?>
 										<?php wp_link_pages('before=<p class="post-pagination"><span>Pages:</span>&after=</p>'); ?>
 									</div>
 									<?php if (is_attachment()) : ?>
-										<p class="category">Author: <?php the_author_posts_link(); ?> &middot; This is an attachment</p>
+										<p class="category">Author: <span class="author vcard"><a class="url fn" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></span> &middot; This is an attachment</p>
 									<?php else : ?>
-										<p class="category">Author: <?php the_author_posts_link(); ?> &middot; Filed under: <?php the_category(', '); ?> <?php the_tags(' &middot; Tags: ', ', ', ''); ?></p>
+										<p class="category">Author: <span class="author vcard"><a class="url fn" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></span> &middot; Filed under: <?php the_category(', '); ?> <?php the_tags(' &middot; Tags: ', ', ', ''); ?></p>
 									<?php endif; ?>
+									
 								</div>
 							</article>
 							
